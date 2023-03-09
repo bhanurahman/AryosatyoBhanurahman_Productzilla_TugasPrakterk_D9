5ac774e03b5a7290e1286cdfad20deba804f1d2f
Aryosatyo Bhanurahman <bhanurahman919@gmail.com>
2023-03-09 20:40:18 +0700

    inisiasi Project

---
+++ tugasPraktek.js
@@ -0,0 +1,33 @@
+const ligaInggris = {
+    peringkat1 : 'emyu',
+    peringkat2 : 'arsenal',
+    peringkat3 : 'city',
+    peringkat4 : 'ipul',
+    peringkat5 : 'bango',
+}
+console.log('Prediksi liga Inggris semoga kejadian ya mas');
+console.log(ligaInggris);
+
+console.log('=====');
+
+const dataDiri = {
+    nama : 'aryo',
+    tempatTinggal : 'Bintara',
+    makananKesukaan : 'telor',
+    minumanKesukaan : 'yang manis-manis',
+    hobi : 'olahraga apaan aja',
+}
+console.log('Data Diri');
+console.log(dataDiri);
+
+console.log('=====');
+
+const diSekitarSaya = {
+    handphone : 'Xiaomi',
+    dompet : 'nyolong hehe',
+    jerseybola : 'king emyu',
+    laptop : 'lenovo',
+    sepatuBola : 'ortis kw',
+}
+console.log('Di Sekitar Saya')
+console.log(diSekitarSaya)
